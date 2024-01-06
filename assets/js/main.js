0498a6c075b29c67dd5524d174b7d854e498a225
tortxoFFoxtrot <155745095+tortxoFFoxtrot@users.noreply.github.com>
2024-01-07 04:34:42 +1100

    flashing cursor in logo and inject github/x links into nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
 (function () {
     pagination(true);
 })();
+(async () => {
+    var ul = document.getElementsByClassName('nav')[0];
+    ul.firstElementChild.firstElementChild.innerText="HOME";
+    var githubli = document.createElement('li');
+    var xli = document.createElement('li');
+    var githuba = document.createElement('a');
+    var xa = document.createElement('a');
+    githuba.appendChild(document.createTextNode('GITHUB'));
+    githuba.target='_blank';
+    githuba.href ='https://github.com/tortxoFFoxtrot';
+    xa.appendChild(document.createTextNode('X'));
+    xa.target='_blank';
+    xa.href ='https://x.com/realfarfoxache';
+    githubli.appendChild(githuba);
+    xli.appendChild(xa);
+    ul.appendChild(githubli);
+    ul.appendChild(xli);
+})();
+const delay = ms => new Promise(res => setTimeout(res, ms));
+(async () => {
+  var bool = true;
+  while (true) {
+    if(!bool)
+    {
+        bool = true;
+        document.getElementById('cursor-span').style.visibility='visible';
+        await delay(820);
+    }
+    else
+    {
+        bool = false;
+        document.getElementById('cursor-span').style.visibility='hidden';
+        await delay(640);
+    }
+  }
+})();
